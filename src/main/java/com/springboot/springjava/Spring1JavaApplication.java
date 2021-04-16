8d2287c76ba023e20e298bcfbb127e21b2cee660
Raphael Feitosa <raphaelcs2@gmail.com>
2021-04-15 22:42:47 -0300

    Created project

---
+++ src/main/java/com/springboot/springjava/Spring1JavaApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.springjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring1JavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring1JavaApplication.class, args);
+	}
+
+}
