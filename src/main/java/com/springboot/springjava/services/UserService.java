483db1e003c935834f4a51cda31da96c97b14028
Raphael Feitosa <raphaelcs2@gmail.com>
2021-04-16 21:56:42 -0300

    User delete

--- src/main/java/com/springboot/springjava/services/UserService.java
+++ src/main/java/com/springboot/springjava/services/UserService.java
@@ -11,23 +11,27 @@ import com.springboot.springjava.repositories.UserRepository;
 
 @Service
 public class UserService {
-	
+
 	@Autowired
 	private UserRepository repository;
-	
+
 	public List<User> findAll() {
-		
+
 		return repository.findAll();
 	}
-	
+
 	public User findById(Long id) {
 		Optional<User> obj = repository.findById(id);
-		
+
 		return obj.get();
 	}
-	
+
 	public User insert(User obj) {
 		return repository.save(obj);
 	}
 
+	public void delete(Long id) {
+
+		repository.deleteById(id);
+	}
 }
